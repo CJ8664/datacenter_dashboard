335a02e8c51d3c6c7eb0b828c9cd30fa0a76a901
Chirag Jain <csjain@ncsu.edu>
2018-10-16 23:39:13 -0400

    First version

--- daemon_server.py
+++ daemon_server.py
@@ -1,9 +1,60 @@
 #!/usr/bin/python
 
-#
+import argparse
+import multiprocessing
+import time
 
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers='152.46.17.34')
+server_count = 100
+server_start = 0
 
-producer.send('test', b'chirag vijay')
+def worker_node():
+    '''
+    The simulation of a single server
+    '''
+    producer = KafkaProducer(bootstrap_servers='152.46.17.34')
+    while True:
+        try:
+            print("Hello" + multiprocessing.current_process().name)
+            producer.send('test', b'chirag vijay')
+            time.sleep(1)
+        except KeyboardInterrupt as ex:
+            break
+
+
+def create_server_farm():
+    '''
+    The function responsible to start threads that will simulate the servers
+    '''
+
+    servers = [ multiprocessing.Process(name='server_{}'.format(server_start + num), target=worker_node) for num in range(server_count)]
+    for server in servers:
+        server.start()
+    for server in servers:
+        server.join()
+
+
+def main():
+    '''
+    Start point of the program and master of all child threaads
+    '''
+    global server_count, server_start, kill_switch
+
+    parser = argparse.ArgumentParser(description='This program generates synthetic data for the Kafka Stream simulating the Server Farm')
+    parser.add_argument('-c', action='store', dest='server_count', help='Server count', required=True, type=int)
+    parser.add_argument('-s', action='store', dest='server_start', help='Server count', required=True, type=int)
+
+    args = parser.parse_args()
+
+    server_count = args.server_count
+    server_start = args.server_start
+
+    try:
+        create_server_farm()
+    except KeyboardInterrupt as ex:
+        print('\nStopping Server Farm')
+
+
+if __name__ == '__main__':
+    main()
