cc18ba2afb7aecaba46a05bc6dce2fc06317339a
Nirav Jain <najain@ncsu.edu>
2018-11-18 23:56:07 -0500

    random data generation

--- daemon_server.py
+++ daemon_server.py
@@ -8,6 +8,35 @@ from kafka import KafkaProducer
 
 server_count = 100
 server_start = 0
+kafka_ips = ['35.211.13.141', '35.211.21.153', '35.211.37.39']
+
+class Metric:
+    def __init__(self, range_low, range_high):
+        self.low = range_low
+        self.high = range_high
+
+    def __repr__(self):
+        return "({}:{})".format(self.low, self.high)
+
+def generate_metric():
+    '''
+    Actual data, seperated by ,
+    '''
+
+    metrics = [
+        Metric(0, 10),
+        Metric(0, 100),
+        Metric(-100, 100),
+        Metric(100, 500),
+        Metric(-200, -100)
+    ]
+
+    #print(metrics)
+    data = [random.randint(m.low, m.high) for m in metrics]
+    endoded_data = ','.join(str(v) for v in data)
+    server_id = multiprocessing.current_process().name
+
+    return "{}:{}".format(server_id, endoded_data)
 
 def worker_node():
     '''
@@ -15,7 +44,7 @@ def worker_node():
     '''
     # ./kafka-topics.sh --create --zookeeper 184.73.102.168:2181,52.5.27.230:2181,54.159.237.81:2181 --replication-factor 3 --partitions 1 --topic test
     # ./kafka-topics.sh --list --zookeeper 184.73.102.168:2181
-    producer = KafkaProducer(bootstrap_servers=['35.211.13.141', '35.211.21.153', '35.211.37.39'], api_version=(2, 0, 0))
+    producer = KafkaProducer(bootstrap_servers=kafka_ips, api_version=(2, 0, 0))
     while True:
         try:
             print("Hello" + multiprocessing.current_process().name)
